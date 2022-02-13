6d9619cc1285638498527ab2c28c0840e5d01854
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-02-12 21:53:07 -0800

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/arrNotes'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = (newNoteObj) => {
+    const request = axios.post(baseUrl, newNoteObj)
+    return request.then(response => response.data)
+}
+
+const update = (id) => {
+    axios.delete(`${baseUrl}/${id}`)
+}
+
+export default {
+    getAll,
+    create,
+    update
+}
